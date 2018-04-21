5541b1d1704c6c22cf494ac1dd1a3cfebaf62d50
Duc Liem Nguyen <cyberliem.civil@gmail.com>
2018-04-21 13:14:20 +0700

    enforce uppercased country code in database interface (#186)

--- stat/stat_storage_test_fw.go
+++ stat/stat_storage_test_fw.go
@@ -137,7 +137,7 @@ func (self *StatStorageTest) TestCountryStats() error {
 	}
 	tzmtStat := common.MetricStatsTimeZone{0: {0: mtStat}}
 	updates := map[string]common.MetricStatsTimeZone{TESTCOUNTRY: tzmtStat}
-	err = self.storage.SetWalletStat(updates, 0)
+	err = self.storage.SetCountryStat(updates, 0)
 	if err != nil {
 		return err
 	}
@@ -377,8 +377,8 @@ func (self *StatStorageTest) TestCountries() error {
 	if len(countries) != 1 {
 		return fmt.Errorf("wrong countries len, expect 1, got %d", len(countries))
 	}
-	if countries[0] != "bunny" {
-		return fmt.Errorf("wrong country result, expect bunny, got %s", countries[0])
+	if countries[0] != "BUNNY" {
+		return fmt.Errorf("wrong country result, expect BUNNY, got %s", countries[0])
 	}
 	return err
 
--- stat/storage/stat_bolt.go
+++ stat/storage/stat_bolt.go
@@ -390,6 +390,7 @@ func (self *BoltStatStorage) GetWalletStats(fromTime uint64, toTime uint64, ethW
 
 func (self *BoltStatStorage) SetCountry(country string) error {
 	var err error
+	country = strings.ToUpper(country)
 	err = self.db.Update(func(tx *bolt.Tx) error {
 		b := tx.Bucket([]byte(COUNTRY_BUCKET))
 		err = b.Put([]byte(country), []byte("1"))
@@ -416,6 +417,7 @@ func (self *BoltStatStorage) SetCountryStat(stats map[string]common.MetricStatsT
 	var err error
 	err = self.db.Update(func(tx *bolt.Tx) error {
 		for country, timeZoneStat := range stats {
+			country = strings.ToUpper(country)
 			b, err := tx.CreateBucketIfNotExists([]byte(country))
 			if err != nil {
 				return err
@@ -466,6 +468,7 @@ func (self *BoltStatStorage) SetCountryStat(stats map[string]common.MetricStatsT
 func (self *BoltStatStorage) GetCountryStats(fromTime, toTime uint64, country string, timezone int64) (common.StatTicks, error) {
 	result := common.StatTicks{}
 	tzstring := fmt.Sprintf("%s%d", TIMEZONE_BUCKET_PREFIX, timezone)
+	country = strings.ToUpper(country)
 	self.db.Update(func(tx *bolt.Tx) error {
 		countryBk, _ := tx.CreateBucketIfNotExists([]byte(country))
 		timezoneBk, _ := countryBk.CreateBucketIfNotExists([]byte(tzstring))
