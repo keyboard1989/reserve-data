f1acb3804e4698d40006f82b1d04145f993c16b4
Victor Tran <vu.tran54@gmail.com>
2018-05-22 21:40:27 +0700

    More feeds on gold - eth and gold - usd (#250)

--- common/global_data.go
+++ common/global_data.go
@@ -22,10 +22,54 @@ type OneForgeGoldData struct {
 	Message   string
 }
 
-type GoldData struct {
-	Timestamp uint64
-	DGX       DGXGoldData
-	OneForge  OneForgeGoldData
+type GDAXGoldData struct {
+	Valid   bool
+	Error   string
+	TradeID uint64 `json:"trade_id"`
+	Price   string `json:"price"`
+	Size    string `json:"size"`
+	Bid     string `json:"bid"`
+	Ask     string `json:"ask"`
+	Volume  string `json:"volume"`
+	Time    string `json:"time"`
 }
 
-// {"value":2.00591,"text":"1 XAU is worth 2.00591 ETH","timestamp":1524811993}
+type KrakenGoldData struct {
+	Valid           bool
+	Error           string        `json:"network_error"`
+	ErrorFromKraken []interface{} `json:"error"`
+	Result          map[string]struct {
+		A []string `json:"a"`
+		B []string `json:"b"`
+		C []string `json:"c"`
+		V []string `json:"v"`
+		P []string `json:"p"`
+		T []uint64 `json:"t"`
+		L []string `json:"l"`
+		H []string `json:"h"`
+		O string   `json:"o"`
+	} `json:"result"`
+}
+
+type GeminiGoldData struct {
+	Valid  bool
+	Error  string
+	Bid    string `json:"bid"`
+	Ask    string `json:"ask"`
+	Volume struct {
+		ETH       string `json:"ETH"`
+		USD       string `json:"USD"`
+		Timestamp uint64 `json:"timestamp"`
+	} `json:"volume"`
+	Last string `json:"last"`
+}
+
+type GoldData struct {
+	Timestamp   uint64
+	DGX         DGXGoldData
+	OneForgeETH OneForgeGoldData
+	OneForgeUSD OneForgeGoldData
+	GDAX        GDAXGoldData
+	Kraken      KrakenGoldData
+	Gemini      GeminiGoldData
+}
--- world/endpoint.go
+++ world/endpoint.go
@@ -7,7 +7,11 @@ import (
 
 type Endpoint interface {
 	GoldDataEndpoint() string
-	BackupGoldDataEndpoint() string
+	OneForgeGoldETHDataEndpoint() string
+	OneForgeGoldUSDDataEndpoint() string
+	GDAXDataEndpoint() string
+	KrakenDataEndpoint() string
+	GeminiDataEndpoint() string
 }
 
 type RealEndpoint struct {
@@ -18,8 +22,24 @@ func (self RealEndpoint) GoldDataEndpoint() string {
 	return "https://datafeed.digix.global/tick/"
 }
 
-func (self RealEndpoint) BackupGoldDataEndpoint() string {
+func (self RealEndpoint) OneForgeGoldETHDataEndpoint() string {
 	return "https://forex.1forge.com/1.0.3/convert?from=XAU&to=ETH&quantity=1&api_key=" + self.OneForgeKey
+}
+
+func (self RealEndpoint) OneForgeGoldUSDDataEndpoint() string {
+	return "https://forex.1forge.com/1.0.3/convert?from=XAU&to=USD&quantity=1&api_key=" + self.OneForgeKey
+}
+
+func (self RealEndpoint) GDAXDataEndpoint() string {
+	return "https://api.gdax.com/products/eth-usd/ticker"
+}
+
+func (self RealEndpoint) KrakenDataEndpoint() string {
+	return "https://api.kraken.com/0/public/Ticker?pair=ETHUSD"
+}
+
+func (self RealEndpoint) GeminiDataEndpoint() string {
+	return "https://api.gemini.com/v1/pubticker/ethusd"
 }
 
 func NewRealEndpointFromFile(path string) (*RealEndpoint, error) {
@@ -39,6 +59,22 @@ func (self SimulatedEndpoint) GoldDataEndpoint() string {
 	return "http://simulator:5400/tick"
 }
 
-func (self SimulatedEndpoint) BackupGoldDataEndpoint() string {
+func (self SimulatedEndpoint) OneForgeGoldETHDataEndpoint() string {
 	return "http://simulator:5500/1.0.3/convert?from=XAU&to=ETH&quantity=1&api_key="
+}
+
+func (self SimulatedEndpoint) OneForgeGoldUSDDataEndpoint() string {
+	return "http://simulator:5500/1.0.3/convert?from=XAU&to=USD&quantity=1&api_key="
+}
+
+func (self SimulatedEndpoint) GDAXDataEndpoint() string {
+	return "http://simulator:5600/products/eth-usd/ticker"
+}
+
+func (self SimulatedEndpoint) KrakenDataEndpoint() string {
+	return "http://simulator:5700/0/public/Ticker?pair=ETHUSD"
+}
+
+func (self SimulatedEndpoint) GeminiDataEndpoint() string {
+	return "http://simulator:5800/v1/pubticker/ethusd"
 }
