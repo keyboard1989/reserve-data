3daf31084a82a2742f875e51ff7b7f9140a11dd3
kyberliem <cyberliem.civil@gmail.com>
2018-05-31 16:44:31 +0700

    fix token setting

--- settings/settings.go
+++ settings/settings.go
@@ -17,27 +17,25 @@ type Settings struct {
 
 var setting Settings
 
-func NewSetting() *Settings {
-	tokensSetting := NewTokenSetting()
-	setting = Settings{tokensSetting}
-	handleEmptyToken()
+func NewSetting(tokenSetting *TokenSetting) *Settings {
+	setting = Settings{tokenSetting}
 	return &setting
 }
 
-func handleEmptyToken() {
-	allToks, err := GetAllTokens()
+func (setting *Settings) HandleEmptyToken(normalPath, simPath string) {
+	allToks, err := setting.GetAllTokens()
 	if err != nil || len(allToks) < 1 {
 		if err != nil {
 			log.Printf("Setting Init: Token DB is faulty (%s), attempt to load token from file", err)
 		} else {
 			log.Printf("Setting Init: Token DB is empty, attempt to load token from file")
 		}
-		tokenPath := TOKEN_DEFAULT_JSON_PATH
+		tokenPath := normalPath
 		if os.Getenv("KYBER_ENV") == "simulation" {
-			tokenPath = TOKEN_DEFAULT_JSON_SIM_PATH
+			tokenPath = simPath
 		}
 
-		if err = LoadTokenFromFile(tokenPath); err != nil {
+		if err = setting.LoadTokenFromFile(tokenPath); err != nil {
 			log.Printf("Setting Init: Can not load Token from file: %s, Token DB is needed to be updated manually", err)
 		}
 	}
--- settings/token_APIs.go
+++ settings/token_APIs.go
@@ -1,63 +1,65 @@
 package settings
 
 import (
+	"encoding/json"
 	"errors"
 	"fmt"
+	"io/ioutil"
 	"log"
 
 	"github.com/KyberNetwork/reserve-data/common"
 	ethereum "github.com/ethereum/go-ethereum/common"
 )
 
-func GetAllTokens() ([]common.Token, error) {
+func (setting *Settings) GetAllTokens() ([]common.Token, error) {
 	return setting.Tokens.Storage.GetAllTokens()
 }
 
-func GetActiveTokens() ([]common.Token, error) {
+func (setting *Settings) GetActiveTokens() ([]common.Token, error) {
 	return setting.Tokens.Storage.GetActiveTokens()
 }
 
-func GetInternalTokens() ([]common.Token, error) {
+func (setting *Settings) GetInternalTokens() ([]common.Token, error) {
 	return setting.Tokens.Storage.GetInternalTokens()
 }
 
-func GetExternalTokens() ([]common.Token, error) {
+func (setting *Settings) GetExternalTokens() ([]common.Token, error) {
 	return setting.Tokens.Storage.GetExternalTokens()
 }
 
-func GetTokenByID(id string) (common.Token, error) {
+func (setting *Settings) GetTokenByID(id string) (common.Token, error) {
 	return setting.Tokens.Storage.GetTokenByID(id)
 }
 
-func GetActiveTokenByID(id string) (common.Token, error) {
+func (setting *Settings) GetActiveTokenByID(id string) (common.Token, error) {
 	return setting.Tokens.Storage.GetActiveTokenByID(id)
 }
 
-func GetInternalTokenByID(id string) (common.Token, error) {
+func (setting *Settings) GetInternalTokenByID(id string) (common.Token, error) {
 	return setting.Tokens.Storage.GetInternalTokenByID(id)
 }
 
-func GetExternalTokenByID(id string) (common.Token, error) {
+func (setting *Settings) GetExternalTokenByID(id string) (common.Token, error) {
 	return setting.Tokens.Storage.GetExternalTokenByID(id)
 }
 
-func GetTokenByAddress(addr ethereum.Address) (common.Token, error) {
+func (setting *Settings) GetTokenByAddress(addr ethereum.Address) (common.Token, error) {
 	return setting.Tokens.Storage.GetTokenByAddress(addr)
 }
 
-func GetActiveTokenByAddress(addr ethereum.Address) (common.Token, error) {
+func (setting *Settings) GetActiveTokenByAddress(addr ethereum.Address) (common.Token, error) {
 	return setting.Tokens.Storage.GetActiveTokenByAddress(addr)
 }
 
-func GetInternalTokenByAddress(addr ethereum.Address) (common.Token, error) {
+func (setting *Settings) GetInternalTokenByAddress(addr ethereum.Address) (common.Token, error) {
 	return setting.Tokens.Storage.GetInternalTokenByAddress(addr)
 }
 
-func GetExternalTokenByAddress(addr ethereum.Address) (common.Token, error) {
+func (setting *Settings) GetExternalTokenByAddress(addr ethereum.Address) (common.Token, error) {
 	return setting.Tokens.Storage.GetExternalTokenByAddress(addr)
 }
 
-func ETHToken() common.Token {
+func (setting *Settings) ETHToken() common.Token {
 	eth, err := setting.Tokens.Storage.GetInternalTokenByID("ETH")
 	if err != nil {
 		log.Panicf("There is no ETH token in token DB, this should not happen (%s)", err)
@@ -65,9 +67,9 @@ func ETHToken() common.Token {
 	return eth
 }
 
-func NewTokenPair(base, quote string) (common.TokenPair, error) {
-	bToken, err1 := GetInternalTokenByID(base)
-	qToken, err2 := GetInternalTokenByID(quote)
+func (setting *Settings) NewTokenPair(base, quote string) (common.TokenPair, error) {
+	bToken, err1 := setting.GetInternalTokenByID(base)
+	qToken, err2 := setting.GetInternalTokenByID(quote)
 	if err1 != nil || err2 != nil {
 		return common.TokenPair{}, errors.New(fmt.Sprintf("%s or %s is not supported", base, quote))
 	} else {
@@ -75,10 +77,32 @@ func NewTokenPair(base, quote string) (common.TokenPair, error) {
 	}
 }
 
-func MustCreateTokenPair(base, quote string) common.TokenPair {
-	pair, err := NewTokenPair(base, quote)
+func (setting *Settings) MustCreateTokenPair(base, quote string) common.TokenPair {
+	pair, err := setting.NewTokenPair(base, quote)
 	if err != nil {
 		panic(err)
 	}
 	return pair
+}
+
+func (setting *Settings) UpdateToken(t common.Token) error {
+	return setting.Tokens.Storage.UpdateToken(t)
+}
+
+func (setting *Settings) LoadTokenFromFile(filePath string) error {
+	data, err := ioutil.ReadFile(filePath)
+	tokens := TokenConfig{}
+	if err != nil {
+		return err
+	}
+	if err = json.Unmarshal(data, &tokens); err != nil {
+		return err
+	}
+	for id, t := range tokens.Tokens {
+		token := common.NewToken(id, t.Name, t.Address, t.Decimal, t.Active, t.Internal, t.MinimalRecordResolution, t.MaxPerBlockImbalance, t.MaxTotalImbalance)
+		if err = setting.UpdateToken(token); err != nil {
+			return err
+		}
+	}
+	return nil
 }
