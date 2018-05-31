ea6a0d31f642b187b7e1f4233627f758831d0061
kyberliem <cyberliem.civil@gmail.com>
2018-05-31 11:58:33 +0700

    add init setting at config

---
+++ cmd/configuration/init_setting.go
@@ -0,0 +1,20 @@
+package configuration
+
+import (
+	"path/filepath"
+
+	"github.com/KyberNetwork/reserve-data/cmd/configuration/mode"
+	"github.com/KyberNetwork/reserve-data/settings"
+)
+
+const (
+	tokenDBFileName             string = "token.db"
+	TOKEN_DEFAULT_JSON_PATH     string = "/go/src/github.com/KyberNetwork/reserve-data/cmd/mainnet_setting.json"
+	TOKEN_DEFAULT_JSON_SIM_PATH string = "/go/src/github.com/KyberNetwork/reserve-data/cmd/shared/deployment_dev.json"
+)
+
+func createSetting() *settings.Settings {
+	tokensSetting := settings.NewTokenSetting(filepath.Join(mode.CmdDirLocation(), tokenDBFileName))
+	setting := settings.NewSetting(tokensSetting)
+	setting.HandleEmptyToken()
+}
