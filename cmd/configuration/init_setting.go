ef697756d876a866a6d6bbe0e15705dc24f89049
kyberliem <cyberliem.civil@gmail.com>
2018-05-31 17:02:31 +0700

    fix hardcode gopath

--- cmd/configuration/init_setting.go
+++ cmd/configuration/init_setting.go
@@ -8,14 +8,14 @@ import (
 )
 
 const (
-	tokenDBFileName             string = "token.db"
-	TOKEN_DEFAULT_JSON_PATH     string = "/go/src/github.com/KyberNetwork/reserve-data/cmd/mainnet_setting.json"
-	TOKEN_DEFAULT_JSON_SIM_PATH string = "/go/src/github.com/KyberNetwork/reserve-data/cmd/shared/deployment_dev.json"
+	tokenDBFileName         string = "token.db"
+	tokenSettingDefaultName string = "mainnet_setting.json"
+	tokenSettingSimName     string = "shared/deployment_dev.json"
 )
 
 func createSetting() *settings.Settings {
 	tokensSetting := settings.NewTokenSetting(filepath.Join(mode.CmdDirLocation(), tokenDBFileName))
 	setting := settings.NewSetting(tokensSetting)
-	setting.HandleEmptyToken(TOKEN_DEFAULT_JSON_PATH, TOKEN_DEFAULT_JSON_SIM_PATH)
+	setting.HandleEmptyToken(filepath.Join(mode.CmdDirLocation(), tokenSettingDefaultName), filepath.Join(mode.CmdDirLocation(), tokenSettingSimName))
 	return setting
 }
